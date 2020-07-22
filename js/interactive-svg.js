d31bdb91e91be17a4a6d1c16c07fac3c8c6b254f
terribedore <33574428+terribedore@users.noreply.github.com>
2020-07-22 15:03:29 -0500

    added corrected file to js

--- js/interactive-svg.js
+++ js/interactive-svg.js
@@ -1,8 +1,8 @@
 $(function(){
-    $("#stage").load('interactive.svg',function(response){
+    $("#stage").load('TerriVenn.svg',function(response){
         $(this).addClass("svgLoaded");
         if(!response){
-            $(this).html('Error loading SVG. Be sure you are running from a the http protocol');
+            $(this).html('Error loading SVG. Be sure you are running from an http protocol');
             // Error loading SVG!
             // Remember, this only works on a web server or localhost!
         }
